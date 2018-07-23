8db41844b5e9d87fff1e9412a870b8739c98c64e
fboot <31297491+YenCChien@users.noreply.github.com>
2018-07-23 17:54:04 +0800

    update
    update

--- rl_atari.py
+++ rl_atari.py
@@ -1,8 +1,17 @@
 import random
 import numpy as np
 import gym
+import matplotlib.pyplot as plt
+import cv2
+from scipy.misc import imresize
 
 env = gym.make('SpaceInvaders-v0')
+
+def preprocess(observation):
+    observation = cv2.cvtColor(cv2.resize(observation, (84, 110)), cv2.COLOR_BGR2GRAY)
+    observation = observation[26:110,:]
+    ret, observation = cv2.threshold(observation,1,255,cv2.THRESH_BINARY)
+    return np.reshape(observation,(84,84,1))
 
 for x in range(10):
     terminal = True
@@ -15,4 +24,7 @@ for x in range(10):
         action_idx = env.action_space.sample()
         state, reward, terminal, _ = env.step(action_idx)
         print('action:', action_idx, 'reward:', reward, len(state[0]), len(state[1]))
-        input('next step')
+        observation0 = preprocess(state)
+        print("After processing: " + str(np.array(observation0).shape))
+        plt.imshow(np.array(np.squeeze(observation0)))
+        plt.show()
